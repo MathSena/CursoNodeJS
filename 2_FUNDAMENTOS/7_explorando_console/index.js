a2d1e37c2778d6aabec04895ad56da2af977b372
Matheus Sena <MathSena07@hotmail.com>
2022-02-04 20:36:00 -0300

    Explorando o console

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,24 @@
+// mais de um valor 
+const x = 10
+const y = 'Algum texto'
+const z = [1,2]
+
+console.log(x, y, z)
+
+
+//contagem de impressões
+console.log(`O valor de x é ${x}, contagem`)
+console.log(`O valor de x é ${x}, contagem`)
+console.log(`O valor de x é ${x}, contagem`)
+console.log(`O valor de x é ${x}, contagem`)
+
+
+//Variavel entre String
+console.log("O nome é %s, ele é programador", y)
+
+
+//Limpar console.log(
+setTimeout(() =>{
+    console.clear()
+
+}, 2000)
