441262969b8bc0fd327fe88fedc84a710e56994a
MathSena <MathSena07@hotmail.com>
2022-02-21 20:25:03 -0300

    Finalizando criação de conta

--- 5_PROJETO_ACCOUNTS/index.js
+++ 5_PROJETO_ACCOUNTS/index.js
@@ -1,4 +1,4 @@
-// modulos externos 
+// modulos externos
 const inquirer = require('inquirer')
 const chalk = require('chalk')
 
@@ -7,31 +7,77 @@ const fs = require('fs')
 
 operation()
 
-function operation(){
-
-    inquirer.prompt([
-        {
+function operation() {
+  inquirer
+    .prompt([
+      {
         type: 'list',
         name: 'action',
         message: 'O que você deseja fazer?',
-        choices: ['Criar conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair'],
-    },
-]).then((answer)=>{
-    const action = answer['action']
+        choices: [
+          'Criar conta',
+          'Consultar Saldo',
+          'Depositar',
+          'Sacar',
+          'Sair'
+        ]
+      }
+    ])
+    .then(answer => {
+      const action = answer['action']
 
-    if(action === 'Criar conta'){
+      if (action === 'Criar conta') {
         createAccount()
-    }
+      }
 
-    console.log(action)
-}).catch((err)=> console.log(err))
+      console.log(action)
+    })
+    .catch(err => console.log(err))
 }
 
 // Create an account
 
-function createAccount(){
-    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco'))
-    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+function createAccount() {
+  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco'))
+  console.log(chalk.green('Defina as opções da sua conta a seguir'))
 
+  buildAccount()
+}
 
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Digite um nome para a sua conta: '
+      }
+    ])
+    .then(answer => {
+      const accountName = answer['accountName']
+
+      console.info(accountName)
+
+      if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts')
+      }
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black('Essa conta já existe, defina um novo nome')
+        )
+        buildAccount()
+        return
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance" : 0}',
+        function (err) {
+          console.log(err)
+        }
+      )
+
+      console.log(chalk.green('Parabéns, conta criada'))
+      operation()
+    })
+    .catch(err => console.log(err))
 }
