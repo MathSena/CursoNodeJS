ebbd535928564fa6fb8e2a680988334f9aa40a80
Matheus Sena <MathSena07@hotmail.com>
2022-02-21 13:59:34 -0300

    Criação de conta

--- 5_PROJETO_ACCOUNTS/index.js
+++ 5_PROJETO_ACCOUNTS/index.js
@@ -19,6 +19,19 @@ function operation(){
 ]).then((answer)=>{
     const action = answer['action']
 
+    if(action === 'Criar conta'){
+        createAccount()
+    }
+
     console.log(action)
 }).catch((err)=> console.log(err))
+}
+
+// Create an account
+
+function createAccount(){
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+
+
 }
